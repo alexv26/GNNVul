0fe5ac4606be2191ee8128d378f6fa09d41626b2
alexv26 <velsmida@bc.edu>
2025-04-24 23:02:18 -0400

    Added json_functions.py

---
+++ utils/json_functions.py
@@ -0,0 +1,13 @@
+import json
+
+def load_json_array(path):
+        try:
+            with open(path, 'r', encoding='utf-8') as file:
+                data = json.load(file)
+        except FileNotFoundError:
+            print("DB not found")
+            data = []
+        return data
+def save_array_to_json(data, path):
+    with open(path, 'w') as json_file:
+        json.dump(data, json_file, indent=4)
